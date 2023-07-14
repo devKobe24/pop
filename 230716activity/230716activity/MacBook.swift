43bea1a2a5a7f19cdfbeb18f82b130b72dd89c78
Kobe <mskang@xingxingmobility.com>
2023-07-15 08:38:54 +0900

    feat: macbook 생성 및 구현

--- 230716activity/230716activity/MacBook.swift
+++ 230716activity/230716activity/MacBook.swift
@@ -8,12 +8,17 @@
 import Foundation
 
 struct MacBook {
-	var permitChargeWattPerHour = 5
-	var currentBettryCapacity = 3
-	var maximumBettryCapcity = 100
+	// 기기 허용 충전 와트시
+	var permitChargeWattPerHour: WattPerHour
+	// 현재 저장된 배터리용량
+	var currentBettryCapacity: WattPerHour
+	// 최대 배터리 용량
+	var maximumBettryCapcity: WattPerHour = 100
 	
 	mutating func chargeBattery(charger: Chargeable) {
-		
+		// 최대 충전 와트시
+		let maximumChargeWattPerHour = charger.convert(chargeableWattPerHour: permitChargeWattPerHour)
+		let result = (maximumBettryCapcity - currentBettryCapacity) / maximumChargeWattPerHour
+		print("\(result)시간 소요되었습니다.")
 	}
-	
 }
