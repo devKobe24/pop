1089de773978ea8cbc9b93fbd3a82ddfb334cf02
Kobe <mskang@xingxingmobility.com>
2023-07-15 07:51:56 +0900

    feat: macbook

---
+++ 230716activity/230716activity/MacBook.swift
@@ -0,0 +1,19 @@
+//
+//  MacBook.swift
+//  230716activity
+//
+//  Created by Minseong Kang on 2023/07/15.
+//
+
+import Foundation
+
+struct MacBook {
+	var permitChargeWattPerHour = 5
+	var currentBettryCapacity = 3
+	var maximumBettryCapcity = 100
+	
+	mutating func chargeBattery(charger: Chargeable) {
+		
+	}
+	
+}
